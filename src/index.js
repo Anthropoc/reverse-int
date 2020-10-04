30eb963113012d3a921e4157641168cb08e9f54b
DESKTOP-O81LPDS\Anthropoc <anthropocmaxi@gmail.com>
2020-10-04 15:33:57 +0300

    feat: modified index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);  
+    let array = str.split("");    
+    let reversed = array.reverse();
+    let joined = reversed.join("");
+    if(joined[joined.length -1] == "-"){
+        joined = joined.slice(0, (joined.length - 1));
+    } else {
+        joined = joined;
+    }
+    let result = joined;
+    return result;
 }
+
